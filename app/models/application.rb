9e73aecfb655be3e77d89f66296ab93e77dd1ae6
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-17 09:52:25 -0500

    Add helper method all_pets_reviewed? Add edge cases to tests

--- app/models/application.rb
+++ app/models/application.rb
@@ -16,8 +16,32 @@ class Application < ApplicationRecord
   def can_approve?
     id = self.id
     app_pets_ids = ApplicationPet.where(application_id: id)
-    app_pets_ids.all? do |app_pet|
-      app_pet.application_status == "Approved"
+    if all_pets_reviewed?
+      app_pets_ids.all? do |app_pet|
+        app_pet.application_status == "Approved"
+      end
+    else
+      false
+    end
+  end
+
+  def can_reject?
+    id = self.id
+    app_pets_ids = ApplicationPet.where(application_id: id)
+    if all_pets_reviewed?
+      app_pets_ids.any? do |app_pet|
+        app_pet.application_status == "Rejected"
+      end
+    else
+      false
+    end
+  end
+
+  def all_pets_reviewed?
+    id = self.id
+    app_pets_ids = ApplicationPet.where(application_id: id)
+    app_pets_ids.none? do |app_pet|
+      app_pet.application_status == "Pending"
     end
   end
 end
