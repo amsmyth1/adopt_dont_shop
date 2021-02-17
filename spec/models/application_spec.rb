a74f32eaa81a62fac3f5a34a4235511c4704270f
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-17 06:58:21 -0500

    Add model test to update applicaiton status to approved

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,20 @@ describe Application, type: :model do
     it { should have_many :application_pets }
     it { should have_many(:pets).through(:application_pets) }
   end
+
+  describe "methods" do
+    describe "#approve" do
+      it "can change an applications status to Approved" do
+        application = create(:application, status: "Pending")
+        pet1 = create(:pet)
+        application.pets << pet1
+        ApplicationPet.approve(pet1.id, application.id)
+
+        expect(application.status).to eq ("Pending")
+        application.approve
+        expect(application.status).to eq ("Approved")
+      end
+    end
+  end
+
 end
