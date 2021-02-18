204ac4024dece53f4c489f701c852af23aef6965
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-18 09:49:12 -0500

    Add test and method for Shelter to return all pending applications

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -5,6 +5,10 @@ class Shelter < ApplicationRecord
     find_by_sql("SELECT * FROM shelters ORDER BY name desc")
   end
 
+  def self.with_pending_applications
+    where(status: "Pending")
+  end
+
   def self.admin_show_query(id)
     find_by_sql("SELECT name, address, city, state, zip FROM shelters WHERE id = #{id}")
   end
