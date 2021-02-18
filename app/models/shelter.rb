1ebebed779308cc456ab64e5f97c15e7407f4d43
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-18 09:13:58 -0500

    Create test for sql methods, all passing

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,11 @@
 class Shelter < ApplicationRecord
   has_many :pets
+
+  def self.all_desc
+    find_by_sql("SELECT * FROM shelters ORDER BY name desc")
+  end
+
+  def self.admin_show_query(id)
+    find_by_sql("SELECT name, address, city, state, zip FROM shelters WHERE id = #{id}")
+  end
 end
--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -1,7 +1,32 @@
 require 'rails_helper'
 
-describe Shelter, type: :model do
+RSpec.describe Shelter, type: :model do
+  before :each do
+    @shelter_1 = create(:shelter, name: "Dogs R Us")
+    @shelter_2 = create(:shelter, name: "Paws n Frands")
+    @shelter_3 = create(:shelter, name: "Animal Patrol")
+  end
+
   describe 'relationships' do
     it { should have_many :pets }
+  end
+
+  describe "::all_desc" do
+    it "should list all shelters in descending order using SQL" do
+      shelters = Shelter.all_desc
+
+      expect(shelters.first.name).to eq(@shelter_2.name)
+      expect(shelters[1].name).to eq(@shelter_1.name)
+      expect(shelters.last.name).to eq(@shelter_3.name)
+    end
+  end
+
+  describe "::admin_show_query" do
+    it "should only query for name and address" do
+      expect(Shelter.admin_show_query(@shelter_1.id).first.name).to eq(@shelter_1.name)
+      expect(Shelter.admin_show_query(@shelter_1.id).first.address).to eq(@shelter_1.address)
+      expect(Shelter.admin_show_query(@shelter_1.id).first.city).to eq(@shelter_1.city)
+      expect(Shelter.admin_show_query(@shelter_1.id).first.state).to eq(@shelter_1.state)
+    end
   end
 end
