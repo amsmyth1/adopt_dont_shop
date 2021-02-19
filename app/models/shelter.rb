da95bc41c6993000c9e6e266d1c589b2b56ff60b
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-19 09:43:52 -0500

    Complete average pet age and count

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -5,16 +5,26 @@ class Shelter < ApplicationRecord
     find_by_sql("SELECT * FROM shelters ORDER BY name desc")
   end
 
-  def self.with_pending_applications
-    pending_apps = Application.all_pending
-    pending_apps.select('applications.*', ' application_pets.pet_id').joins(:application_pets)
-  end
+  # def self.with_pending_applications
+  #   pending_apps = Application.all_pending
+  #   pending_apps.select('applications.*', ' application_pets.pet_id').joins(:application_pets)
+  # end
 
   def self.admin_show_query(id)
-    find_by_sql("SELECT name, address, city, state, zip FROM shelters WHERE id = #{id}")
+    find_by_sql("SELECT id, name, address, city, state, zip FROM shelters WHERE id = #{id}")
   end
 
-  def self.shelters_with_pending_applications
-    Pet.shelters_with_pending_applications.joins(:shelters)
+  def average_pet_age
+    pets = Pet.where(shelter_id: self.id)
+    pets.average(:approximate_age)
   end
+
+  def pet_count
+    pets = Pet.where(shelter_id: self.id)
+    pets.count
+  end
+
+  # def self.shelters_with_pending_applications
+  #   Pet.shelters_with_pending_applications.joins(:shelters)
+  # end
 end
--- spec/features/admin_shelters/show_spec.rb
+++ spec/features/admin_shelters/show_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "ADMIN shelter show page" do
+  before :each do
+    @shelter = create(:shelter, name: "Fur and Feathers")
+    @pet1 = create(:pet, approximate_age: 1, shelter_id: @shelter.id)
+    @pet2 = create(:pet, approximate_age: 2, shelter_id: @shelter.id)
+    @pet3 = create(:pet, approximate_age: 3, shelter_id: @shelter.id)
+    @pet4 = create(:pet, approximate_age: 4, shelter_id: @shelter.id)
+    @pet5 = create(:pet, approximate_age: 5, shelter_id: @shelter.id)
+    @average_age = ((1+ 2 + 3 + 4 + 5) / 5)
+
+  end
   describe "should display the shelters" do
     it "only displays name and full address" do
       shelter = create(:shelter)
@@ -11,6 +21,25 @@ RSpec.describe "ADMIN shelter show page" do
       expect(page).to have_content(shelter.city)
       expect(page).to have_content(shelter.state)
       expect(page).to have_content(shelter.zip)
+    end
+  end
+
+  describe "should have a section for shelter statistics" do
+    it "displays the average pet age of the shelter" do
+
+      visit "/admin/shelters/#{@shelter.id}"
+
+      within ".admin_shelter_info#statistics" do
+        expect(page).to have_content(@average_age)
+      end
+    end
+
+    it "displays the pet count of the shelter" do
+      visit "/admin/shelters/#{@shelter.id}"
+
+      within ".admin_shelter_info#statistics" do
+        expect(page).to have_content("Count of Adoptable: 5")
+      end
     end
   end
 end
