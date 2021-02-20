0353a32ce730d427d33b2c829fedc7abb6d117fe
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-20 11:45:08 -0500

    Refactor to use more active record

--- app/models/application.rb
+++ app/models/application.rb
@@ -28,9 +28,8 @@ class Application < ApplicationRecord
   end
 
   def can_reject?
-    number_of_pets_on_app = ApplicationPet.where(application_id: self.id).count
     number_of_pets_rejected_on_app = ApplicationPet.where(application_id: self.id).where('application_status = ?', 'Rejected').count
-    if all_pets_reviewed? && number_of_pets_on_app == number_of_pets_rejected_on_app
+    if number_of_pets_rejected_on_app > 0
       true
     else
       false
@@ -48,21 +47,10 @@ class Application < ApplicationRecord
   end
 
   def adopt_all_pets
-    id = self.id
-    app_pets_ids = ApplicationPet.where(application_id: id)
-    app_pets_ids.each do |app_pet|
-      pet = Pet.find(app_pet.pet_id)
-      pet.update(adoptable: false)
-      # pet.adopt
-      ApplicationPet.other_applications_with_pet(app_pet.pet_id, id)
-    end
+    Pet.joins(:applications).where('applications.id = ?', self.id).update(adoptable: false)
   end
 
   def self.all_pending
     pending = Application.where(status: "Pending")
   end
-
-  # def self.shelter_associations(shelt_id)
-  #   all_pending.joins(:application_pets).joins(:pets).where('pets.shelter_id = ?', shelt_id).select('id').distinct
-  # end
 end
