6c2533c1d301b1fdd6bfaedab969d41c5135fd3f
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-20 09:43:19 -0500

    Remove commented out code

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -2,7 +2,7 @@ class AdminSheltersController < ApplicationController
 
   def index
     @shelters = Shelter.all_desc
-    @shelters_pending_apps = Pet.shelters_with_pending_applications
+    @shelters_pending_apps = Shelter.shelters_with_pending_applications
   end
 
   def show
