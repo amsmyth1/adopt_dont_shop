a71fa7e2f31e740f66afcdde45d17a14582888da
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-23 12:11:10 -0500

    Complete ar refactor for all user stories

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -10,6 +10,7 @@ class AdminSheltersController < ApplicationController
     @shelter_average_pet_age = @shelter.average_pet_age
     @shelter_pet_count = @shelter.pet_count
     @shelter_number_adopted_pets = @shelter.adopted_pet_count
-    @shelter_pets_not_reviewed_on_apps = @shelter.pets_pending_app_reivew
+
+    @shelter_pets_not_reviewed_on_apps = @shelter.pets_pending_app_reivew.distinct(:name)
   end
 end
